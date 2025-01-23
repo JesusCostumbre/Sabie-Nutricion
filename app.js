75b74c6334caa8fab5312bd43d99670f59424b4a
Jesus Costumbre <jesuscostumbre11@gmail.com>
2025-01-22 23:40:25 -0600

    Terminamos de el menu desplegable y arreglamos detalles de los layouts

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-const toggleMenuOpen = () => document.body.classList.toggle("open");
+function toggleMenuOpen() {
+    document.body.classList.toggle('open');
+  }
+  
+  // Cerrar el menú al hacer clic en cualquier enlace del menú
+  document.querySelectorAll('.navbar-menu button').forEach(button => {
+    button.addEventListener('click', () => {
+      document.body.classList.remove('open');
+    });
+  });
+  
+  // Cerrar el menú al hacer clic en cualquier parte fuera del menú
+  document.addEventListener('click', function(event) {
+    const navbarMenu = document.querySelector('.navbar-menu');
+    const navbarBurger = document.querySelector('.navbar-burger');
+    const navbarOverlay = document.querySelector('.navbar-overlay');
+  
+    if (document.body.classList.contains('open') && 
+        !navbarMenu.contains(event.target) && 
+        !navbarBurger.contains(event.target) &&
+        !navbarOverlay.contains(event.target)) {
+      document.body.classList.remove('open');
+    }
+  });
+  
